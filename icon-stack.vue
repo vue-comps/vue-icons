366760717ecbd628b9816bd44683c719bd7f78e6
Paul Pflugradt <paulpflugradt@googlemail.com>
2016-08-22 12:57:33 +0200

    Updates

---
+++ icon-stack.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+.container(style="font-size:12pt")
+  p fa-ban(scale=2,style="color:red") on fa-camera:
+    icon(name="fa-camera")
+      icon-stack(name="fa-ban",style="color:red",scale=2)
+  p fa-beer on fa-thumbs-up (with offset-x / offset-y and scale):
+    icon(name="fa-thumbs-up",offset-x=-30)
+      icon-stack(name="fa-beer",offset-x=30,scale=1.2,offset-y=-20)
+  a(href="https://github.com/vue-comps/vue-icons/blob/master/dev/basic.vue") source
+</template>
+
+<script lang="coffee">
+module.exports =
+  components:
+    "icon" : require "../icon.js"
+    "icon-stack" : require "../icon-stack.js"
+
+</script>
